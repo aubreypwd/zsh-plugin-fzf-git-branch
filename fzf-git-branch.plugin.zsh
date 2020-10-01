d6883a3c894b8570cdac3bf7c6944e918fff0fad
Aubrey Portwood <code@aubreypwd.com>
2020-10-01 09:46:23 -0600

    Log the required commands.

--- fzf-git-branch.plugin.zsh
+++ fzf-git-branch.plugin.zsh
@@ -7,13 +7,35 @@
  # @since 1.0.0
  ##
 function fzf-git-branch {
-	local branches=$(git branch -a) &&
-	local branch=$(echo "$branches" | fzf +s +m -e) &&
-	local cmd=$(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
-
-	if [ -z "$cmd" ]; then
+	if ! [[ -x $(command -v fzf) ]]; then
+		echo "Please install fzf:";
+		echo "\tHomebrew: brew install fzf"
 		return
 	fi
 
-	git checkout "$cmd"
+	if ! [[ -x $(command -v sed) ]]; then
+		echo "Please install sed";
+		return
+	fi
+
+	if ! [[ -x $(command -v git) ]]; then
+		echo "Please install git:";
+		echo "\tHomebrew: brew install git"
+		return
+	fi
+
+	if ! [[ -d ".git" ]]; then
+		echo "Not a git repository."
+		return
+	fi
+
+	local branches=$(git branch -a) &&
+	local branch=$(echo "$branches" | fzf +s +m -e) &&
+	local git_cmd=$(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
+
+	if [ -z "$git_cmd" ]; then
+		return
+	fi
+
+	git checkout "$git_cmd"
 }
