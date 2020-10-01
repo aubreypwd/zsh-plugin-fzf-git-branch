6da81678436f9fa73f2a81513bc92c78a7d293ec
Aubrey Portwood <aubreypwd@users.noreply.github.com>
2020-10-01 09:30:29 -0600

    Add the original version from .config

--- plugin-slug.plugin.zsh
+++ plugin-slug.plugin.zsh
@@ -1,7 +1,19 @@
 #!/bin/zsh
 
 ###
- # My Plugin
+ # Checkout a branch using fzf.
  #
+ # @since 5/15/17
  # @since 1.0.0
  ##
+function fzf-git-branch {
+	local branches=$(git branch -a) &&
+	local branch=$(echo "$branches" | fzf +s +m -e) &&
+	local cmd=$(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
+
+	if [ -z "$cmd" ]; then
+		return
+	fi
+
+	git checkout "$cmd"
+}
